5abffd5d838ba0c381a7f43e5cd75519bff087d0
Bensosam <bsosam1@miumg.edu.gt>
2024-02-21 22:39:14 -0600

    primer_commit

---
+++ Estudiante.cpp
@@ -0,0 +1,26 @@
+#include "Persona.cpp"
+#include <iostream>
+// comentario en la calse persona
+using namespace std;
+class Estudiante : Persona {
+	// atributos 
+	private : string carnet;
+	// costructor
+	public :
+		Estudiante(){
+		}
+		Estudiante(string nom,string ape,string dir,int tel,string fn,string car) 
+				: Persona(nom,ape,dir,tel,fn){
+					carnet = car;
+		}
+	// metodos
+	void leer(){
+		cout<<carnet<<endl;
+		cout<<nombres<<endl;
+		cout<<apellidos<<endl;
+		cout<<direccion<<endl;
+		cout<<telefono<<endl;
+		cout<<fecha_nacimiento<<endl;
+	}
+	
+};
